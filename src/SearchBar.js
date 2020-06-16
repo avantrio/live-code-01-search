44d6a2a8a57f6a3edb53ece7a74692a55aad22c0
nipuna rivimal <nipuna.rivimal@gmail.com>
2020-06-16 15:11:52 +0530

    axios get

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,26 +1,32 @@
 import React, { useState, useEffect } from 'react';
 import Constants from './Constants';
+import Results from './ResultsComponent';
 import axios from 'axios'
 
 function SearchBar() {
 
-    const [searchQuery, setSearchQuery] = useState('samsung');
+    const [searchQuery, setSearchQuery] = useState('');
     const [url, setUrl] = useState(Constants.BASEapi);
     const [token, setToken] = useState("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjo0LCJ1c2VybmFtZSI6Im5pcHVuYSIsImV4cCI6MTU5MjMwMDU1MSwiZW1haWwiOiIiLCJvcmlnX2lhdCI6MTU5MjI5Njk1MX0.3LzDNysr_2OygEuEGVaLRM7hZ66Skgw8W-nidMRoDEQ");
     const [list, setList] = useState([]);
 
-    useEffect(() => {
-        onSearch();
-    }, [])
+    // useEffect(() => {
+    //    // onSearch();
+    // }, [searchQuery]);
+
+
     const onSearchCahnge = (e) => {
         setSearchQuery(e.target.value);
-        console.log(searchQuery);
+        if (e.target.value.length > 3) {
+            onSearch(e.target.value);
+        }
+
 
     }
 
-    const onSearch = () => {
+    const onSearch = (searchInput) => {
 
-        axios.get("http://161.35.44.217:8080/api/devices?search=samsung", {
+        axios.get(`http://161.35.44.217:8080/api/devices?search=${searchInput}`, {
             headers: {
                 Authorization: 'Bearer ' + token
             }
@@ -29,18 +35,19 @@ function SearchBar() {
 
             setList(res.data.data);
 
-        })
+        }).catch(error => console.log(error)
+        )
 
 
 
     }
-    console.log(list);
     return (
         <div class="input-group mb-3" style={{ marginTop: 50 }}>
             <input type="text" class="form-control" placeholder="Search..." aria-label="Recipient's username" aria-describedby="basic-addon2" onChange={onSearchCahnge} />
             <div class="input-group-append">
-                <button class="btn btn-outline-secondary" type="button">Button</button>
+                <button class="btn btn-outline-secondary" type="button" onClick={onSearch}>Button</button>
             </div>
+            <Results results={list} />
         </div>
     )
 }
