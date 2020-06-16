265836a9edff051efd87dad41faa4a87be125684
manzzup <manujith.nc@gmail.com>
2020-06-16 14:08:08 +0530

    added results component

--- avantrio-search/src/Search.jsx
+++ avantrio-search/src/Search.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import TextField from '@material-ui/core/TextField';
 import Button from "@material-ui/core/Button";
+import ResultsComponent from './ResultsComponent';
 
 class Search extends Component {
     constructor(props) {
@@ -32,6 +33,62 @@ class Search extends Component {
             />
 
             <Button onClick={() => {this.handleSearchClicked()}}>Search</Button>
+
+            <ResultsComponent 
+                results={
+                    [
+                        {
+                          "id": 1,
+                          "state": "Finalized",
+                          "phone_type": "High End",
+                          "name": "Samsung Gaalxy",
+                          "model": "S10"
+                        },
+                        {
+                            "id": 2,
+                            "state": "Released",
+                            "phone_type": "High End",
+                            "name": "Samsung",
+                            "model": "A10"
+                        },
+                        {
+                            "id": 683435089,
+                            "state": "finalized",
+                            "phone_type": "Phone",
+                            "name": "Samsung Galaxy Note 4Samsung Galaxy Note 4Samsung Galaxy Note 4",
+                            "model": "SM-N910T"
+                        },
+                        {
+                            "id": 683435090,
+                            "state": "finalized",
+                            "phone_type": "Phone",
+                            "name": "Samsung Galaxy Note 4Samsung Galaxy Note 4Samsung Galaxy Note 4",
+                            "model": "SM-N910V"
+                        },
+                        {
+                            "id": 683435092,
+                            "state": "finalized",
+                            "phone_type": "Phone",
+                            "name": "Sprint Cell PhonesSamsung Galaxy Note 4Samsung Galaxy Note 4 Sprint",
+                            "model": "SM-N910P"
+                        },
+                        {
+                            "id": 683435093,
+                            "state": "finalized",
+                            "phone_type": "Phone",
+                            "name": "Samsung Galaxy Note 4Samsung Galaxy Note 4Samsung Galaxy Note 4",
+                            "model": "SAMSUNG-SM-N910A"
+                        },
+                        {
+                            "id": 683435100,
+                            "state": "finalized",
+                            "phone_type": "Phone",
+                            "name": "Samsung Galaxy AvantSamsung Galaxy AvantSamsung Galaxy Avant",
+                            "model": "SM-G386T"
+                        }
+                    ]
+                }
+            />
         </div>
         );
     }
