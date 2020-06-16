f0d53cc59c561065aac6d0a9aea52c72a83f956b
nipuna rivimal <nipuna.rivimal@gmail.com>
2020-06-16 13:44:54 +0530

    Search bar

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -3,9 +3,15 @@ import React, { useState } from 'react';
 function SearchBar() {
 
     const [searchQuery, setSearchQuery] = useState('');
+
+    const onSearchCahnge = (e) => {
+        setSearchQuery(e.target.value);
+        console.log(searchQuery);
+
+    }
     return (
         <div class="input-group mb-3">
-            <input type="text" class="form-control" placeholder="Search..." aria-label="Recipient's username" aria-describedby="basic-addon2" />
+            <input type="text" class="form-control" placeholder="Search..." aria-label="Recipient's username" aria-describedby="basic-addon2" onChange={onSearchCahnge} />
             <div class="input-group-append">
                 <button class="btn btn-outline-secondary" type="button">Button</button>
             </div>
